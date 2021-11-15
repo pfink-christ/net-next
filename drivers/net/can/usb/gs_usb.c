pfink-christ/net-next

commit c7daf3ede34b049aea32700b0face52210b6aea4

can: gs_usb: add FD support for CANtact Pro
CANtact Pro from Linklayer added support for CAN FD
using a different HW and re-written candlelight FW.
Code from https://github.com/linklayer/gs_usb_fd
rebased onto current master.

Signed-off-by: Peter Fink <pfink@christ-es.de>

drivers/net/can/usb/gs_usb.c
@@ -40,6 +40,7 @@ enum gs_usb_breq {
 	GS_USB_BREQ_DEVICE_CONFIG,
 	GS_USB_BREQ_TIMESTAMP,
 	GS_USB_BREQ_IDENTIFY,
+	GS_USB_BREQ_DATA_BITTIMING,
 };
 
 enum gs_can_mode {
@@ -92,6 +93,7 @@ struct gs_device_config {
 #define GS_CAN_MODE_LOOP_BACK            BIT(1)
 #define GS_CAN_MODE_TRIPLE_SAMPLE        BIT(2)
 #define GS_CAN_MODE_ONE_SHOT             BIT(3)
+#define GS_CAN_MODE_FD                   BIT(8)
 
 struct gs_device_mode {
 	__le32 mode;
@@ -122,6 +124,7 @@ struct gs_identify_mode {
 #define GS_CAN_FEATURE_ONE_SHOT         BIT(3)
 #define GS_CAN_FEATURE_HW_TIMESTAMP     BIT(4)
 #define GS_CAN_FEATURE_IDENTIFY         BIT(5)
+#define GS_CAN_FEATURE_FD               BIT(8)
 
 struct gs_device_bt_const {
 	__le32 feature;
@@ -136,7 +139,10 @@ struct gs_device_bt_const {
 	__le32 brp_inc;
 } __packed;
 
-#define GS_CAN_FLAG_OVERFLOW 1
+#define GS_CAN_FLAG_OVERFLOW        BIT(0)
+#define GS_CAN_FLAG_FD              BIT(1)
+#define GS_CAN_FLAG_BRS             BIT(2)
+#define GS_CAN_FLAG_ESI             BIT(3)
 
 struct gs_host_frame {
 	u32 echo_id;
@@ -147,7 +153,7 @@ struct gs_host_frame {
 	u8 flags;
 	u8 reserved;
 
-	u8 data[8];
+	u8 data[64];
 } __packed;
 /* The GS USB devices make use of the same flags and masks as in
  * linux/can.h and linux/can/error.h, and no additional mapping is necessary.
@@ -304,6 +310,7 @@ static void gs_usb_receive_bulk_callback(struct urb *urb)
 	struct gs_host_frame *hf = urb->transfer_buffer;
 	struct gs_tx_context *txc;
 	struct can_frame *cf;
+	struct canfd_frame *cfd;
 	struct sk_buff *skb;
 
 	BUG_ON(!usbcan);
@@ -332,18 +339,32 @@ static void gs_usb_receive_bulk_callback(struct urb *urb)
 		return;
 
 	if (hf->echo_id == -1) { /* normal rx */
-		skb = alloc_can_skb(dev->netdev, &cf);
-		if (!skb)
-			return;
+		if (hf->flags & GS_CAN_FLAG_FD) {
+			skb = alloc_canfd_skb(dev->netdev, &cfd);
+			if (!skb)
+				return;
 
-		cf->can_id = le32_to_cpu(hf->can_id);
+			cfd->can_id = le32_to_cpu(hf->can_id);
+			cfd->len = can_fd_dlc2len(hf->can_dlc);
+			if (hf->flags & GS_CAN_FLAG_BRS) {
+				cfd->flags |= CANFD_BRS;
+			}
+			if (hf->flags & GS_CAN_FLAG_ESI) {
+				cfd->flags |= CANFD_ESI;
+			}
+			memcpy(cfd->data, hf->data, cfd->len);
+		} else {
+			skb = alloc_can_skb(dev->netdev, &cf);
+			if (!skb)
+				return;
+			cf->can_id = le32_to_cpu(hf->can_id);
+			can_frame_set_cc_len(cf, hf->can_dlc, dev->can.ctrlmode);
+			memcpy(cf->data, hf->data, 8);
 
-		can_frame_set_cc_len(cf, hf->can_dlc, dev->can.ctrlmode);
-		memcpy(cf->data, hf->data, 8);
-
-		/* ERROR frames tell us information about the controller */
-		if (le32_to_cpu(hf->can_id) & CAN_ERR_FLAG)
-			gs_update_state(dev, cf);
+			/* ERROR frames tell us information about the controller */
+			if (le32_to_cpu(hf->can_id) & CAN_ERR_FLAG)
+				gs_update_state(dev, cf);
+		}
 
 		netdev->stats.rx_packets++;
 		netdev->stats.rx_bytes += hf->can_dlc;
@@ -451,6 +472,44 @@ static int gs_usb_set_bittiming(struct net_device *netdev)
 	return (rc > 0) ? 0 : rc;
 }
 
+static int gs_usb_set_data_bittiming(struct net_device *netdev)
+{
+	struct gs_can *dev = netdev_priv(netdev);
+	struct can_bittiming *bt = &dev->can.bittiming;
+	struct usb_interface *intf = dev->iface;
+	int rc;
+	struct gs_device_bittiming *dbt;
+
+	dbt = kmalloc(sizeof(*dbt), GFP_KERNEL);
+	if (!dbt)
+		return -ENOMEM;
+
+	dbt->prop_seg = cpu_to_le32(bt->prop_seg);
+	dbt->phase_seg1 = cpu_to_le32(bt->phase_seg1);
+	dbt->phase_seg2 = cpu_to_le32(bt->phase_seg2);
+	dbt->sjw = cpu_to_le32(bt->sjw);
+	dbt->brp = cpu_to_le32(bt->brp);
+
+	/* request bit timings */
+	rc = usb_control_msg(interface_to_usbdev(intf),
+			     usb_sndctrlpipe(interface_to_usbdev(intf), 0),
+			     GS_USB_BREQ_DATA_BITTIMING,
+			     USB_DIR_OUT | USB_TYPE_VENDOR | USB_RECIP_INTERFACE,
+			     dev->channel,
+			     0,
+			     dbt,
+			     sizeof(*dbt),
+			     1000);
+
+	kfree(dbt);
+
+	if (rc < 0)
+		dev_err(netdev->dev.parent, "Couldn't set data bittimings (err=%d)",
+			rc);
+
+	return (rc > 0) ? 0 : rc;
+}
+
 static void gs_usb_xmit_callback(struct urb *urb)
 {
 	struct gs_tx_context *txc = urb->context;
@@ -474,6 +533,7 @@ static netdev_tx_t gs_can_start_xmit(struct sk_buff *skb,
 	struct urb *urb;
 	struct gs_host_frame *hf;
 	struct can_frame *cf;
+	struct canfd_frame *cfd;
 	int rc;
 	unsigned int idx;
 	struct gs_tx_context *txc;
@@ -507,13 +567,28 @@ static netdev_tx_t gs_can_start_xmit(struct sk_buff *skb,
 
 	hf->echo_id = idx;
 	hf->channel = dev->channel;
+	hf->flags = 0;
 
-	cf = (struct can_frame *)skb->data;
+	if (skb->len == CANFD_MTU) {
+		cfd = (struct canfd_frame *)skb->data;
+		hf->can_id = cpu_to_le32(cfd->can_id);
 
-	hf->can_id = cpu_to_le32(cf->can_id);
-	hf->can_dlc = can_get_cc_dlc(cf, dev->can.ctrlmode);
+		hf->flags |= GS_CAN_FLAG_FD;
+		if (cfd->flags & CANFD_BRS) {
+			hf->flags |= GS_CAN_FLAG_BRS;
+		}
+		if (cfd->flags & CANFD_ESI) {
+			hf->flags |= GS_CAN_FLAG_ESI;
+		}
 
-	memcpy(hf->data, cf->data, cf->len);
+		hf->can_dlc = can_fd_len2dlc(cfd->len);
+		memcpy(hf->data, cfd->data, cfd->len);
+	} else {
+		cf = (struct can_frame *)skb->data;
+		hf->can_id = cpu_to_le32(cf->can_id);
+		hf->can_dlc = can_get_cc_dlc(cf, dev->can.ctrlmode);
+		memcpy(hf->data, cf->data, cf->len);
+	}
 
 	usb_fill_bulk_urb(urb, dev->udev,
 			  usb_sndbulkpipe(dev->udev, GSUSB_ENDPOINT_OUT),
@@ -663,6 +738,9 @@ static int gs_can_open(struct net_device *netdev)
 
 	if (ctrlmode & CAN_CTRLMODE_3_SAMPLES)
 		flags |= GS_CAN_MODE_TRIPLE_SAMPLE;
+
+	if (ctrlmode & CAN_CTRLMODE_FD)
+		flags |= GS_CAN_MODE_FD;
 
 	/* finally start device */
 	dm->mode = cpu_to_le32(GS_CAN_MODE_START);
@@ -867,6 +945,8 @@ static struct gs_can *gs_make_candev(unsigned int channel,
 	dev->can.clock.freq = le32_to_cpu(bt_const->fclk_can);
 	dev->can.bittiming_const = &dev->bt_const;
 	dev->can.do_set_bittiming = gs_usb_set_bittiming;
+	dev->can.data_bittiming_const = &dev->bt_const;
+	dev->can.do_set_data_bittiming = gs_usb_set_data_bittiming;
 
 	dev->can.ctrlmode_supported = CAN_CTRLMODE_CC_LEN8_DLC;
 
@@ -882,6 +962,9 @@ static struct gs_can *gs_make_candev(unsigned int channel,
 
 	if (feature & GS_CAN_FEATURE_ONE_SHOT)
 		dev->can.ctrlmode_supported |= CAN_CTRLMODE_ONE_SHOT;
+
+	if (feature & GS_CAN_FEATURE_FD)
+		dev->can.ctrlmode_supported |= CAN_CTRLMODE_FD;
 
 	SET_NETDEV_DEV(netdev, &intf->dev);
 

commit 35978eaccd333f4fa95db5dfddb2044dd4c9378a

can: gs_usb: fix support for candlelight firmware
Support for non FD candlelight firmware was broken with
c7daf3ede34b: ("can: gs_usb: add FD support for CANtact Pro")
which increases the size of gs_host_frame.data from 8 to 64
bytes. In this case gs_can_start_xmit fails when trying to
send data. Send only 8 bytes if the firmware does not have
the feature GS_CAN_FEATURE_FD.

Fixes: c7daf3ede34b: ("can: gs_usb: add FD support for CANtact Pro")
Signed-off-by: Peter Fink <pfink@christ-es.de>
Tested-by: Christoph Möhring <cmoehring@christ-es.de>

drivers/net/can/usb/gs_usb.c
@@ -1008,10 +1008,14 @@ static struct gs_can *gs_make_candev(unsigned int channel,
 	if (feature & GS_CAN_FEATURE_ONE_SHOT)
 		dev->can.ctrlmode_supported |= CAN_CTRLMODE_ONE_SHOT;
 
+	dev->gs_hf_xmit_size = sizeof(struct gs_host_frame);
+
 	if (feature & GS_CAN_FEATURE_FD)
 		dev->can.ctrlmode_supported |= CAN_CTRLMODE_FD;
+	else
+		/* only transfer 8 data bytes instead of 64 */
+		dev->gs_hf_xmit_size -= 56 * sizeof(u8);
 
-	dev->gs_hf_xmit_size = sizeof(struct gs_host_frame);
 
 	if (!(feature & GS_CAN_FEATURE_REQ_USB_QUIRK))
 		/* don't transfer the dummy byte if not requested */